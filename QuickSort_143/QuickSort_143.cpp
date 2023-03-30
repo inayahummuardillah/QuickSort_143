5cdfe648be99b61b665d14a2206e9b065e779deb
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-03-30 14:45:43 +0630

    fungsi while

--- QuickSort_143/QuickSort_143.cpp
+++ QuickSort_143/QuickSort_143.cpp
@@ -6,3 +6,20 @@ int arr[20];                   //array of integers to hold velues
 int cmp_count = 0;             //number of comparasion
 int mov_count = 0;             //number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang elemnet array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n==================" << endl;
+	cout << "\nEnter Array Elemet" << endl;
+	cout << "\n------------------" << endl;
+}
