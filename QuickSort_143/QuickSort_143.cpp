f00d2d9f447ef51d12835db27d53072849a86f51
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-03-30 15:41:45 +0630

    int main

--- QuickSort_143/QuickSort_143.cpp
+++ QuickSort_143/QuickSort_143.cpp
@@ -111,3 +111,13 @@ void display() {
 	cout << "\n\nNumber of comparasion : " << cmp_count << endl;
 	cout << "Number of data movement : " << mov_count << endl;
 }
+
+int main()
+{
+	input();
+	q_short(0, n - 1);              //sort the array using quick sort
+	display();
+	system("pause");
+
+	return 0;
+}
